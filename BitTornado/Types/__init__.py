1b5ccf3616f6181ab938baf8481cd7c8ce11a0aa
Christopher J. Markiewicz <effigies@gmail.com>
2016-08-11 00:26:15 -0400

    RF: Add constrained types, aliases

---
+++ BitTornado/Types/__init__.py
@@ -0,0 +1,2 @@
+from .primitives import *
+from .aliases import *
---
+++ BitTornado/Types/aliases.py
@@ -0,0 +1,17 @@
+from .primitives import TwentyBytes, UnsignedShort
+
+
+class Infohash(TwentyBytes):
+    """SHA-1 hash digest"""
+
+
+class PeerID(TwentyBytes):
+    """Unique peer identifier"""
+
+
+class Port(UnsignedShort):
+    """TCP/IP port number ranging from 0 to 65535"""
+
+
+class Path(str):
+    """File location"""
